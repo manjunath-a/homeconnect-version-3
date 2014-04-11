42c65da88695df46b6d19871ee3f3a46ab5a8cea
shan <shan@localhost>
2014-04-11 15:52:11 +0530

    Update in Partnet Connect API - included Special Price

--- hcoget.php
+++ hcoget.php
@@ -1,8 +1,9 @@
 <?php
-    if($_REQUEST)
+   if($_REQUEST)
         $parameters=$_REQUEST;
     else
         die('No parameters');
+   
     $arraycount=1;
     $masterProductList=array();
     
@@ -10,7 +11,7 @@
     try
     {
         $proxy = new SoapClient('http://www.homeconnectonline.com/index.php/api/soap/?wsdl'); 
-        $sessionId = $proxy->login($_REQUEST['user'], $_REQUEST['key']);
+        $sessionId = $proxy->login($_REQUEST['user'], $_REQUEST['key']);                 
     
         $categoryId = $_REQUEST['catid']; // Put here your category id (offers)
         $storeId = 0;
@@ -22,8 +23,8 @@
         foreach($listOfferProducts as $productList)
         {
             $productInfo = $proxy->call($sessionId,'catalog_product.info',$productList['product_id']);
-            /* echo "<pre>";
-            //print_r($productInfo);
+            /*echo "<pre>";
+            print_r($productInfo);
             echo "--------------<br/><br/>";
             echo "</pre>";*/
     
@@ -40,10 +41,21 @@
             $productName = $productInfo['name'];
             $productDescription = $productInfo['short_description'];
             $productPrice = $productInfo['price'];
+            $productSpecialPrice = $productInfo['special_price'];
+            /*echo "<pre>";
+            print_r($productImage);
+            echo "</pre>";
+            echo "<br/><br/>-----";
+            echo count($productImage);
+            echo "<br/><br/>-----";
+             * 
+             */
             if(count($productImage)==0)
                 $productImageUrl='http://i.imgur.com/RoyW0oD.jpg';
             else
                 $productImageUrl = $productImage[0]['url'];
+            //if($productImageUrl==null)
+            //    $productImageUrl='http://i.imgur.com/RoyW0oD.jpg';
             $productPageUrl = $productInfo['url_path'];
             $productCategory=$productInfo['categories'];
             foreach($productCategory as $category)
@@ -61,18 +73,19 @@
             $productDetail['name']=$productName;
             $productDetail['short_description']=$productDescription;
             $productDetail['price']=$productPrice;
+            $productDetail['special_price']=$productSpecialPrice;
             $productDetail['image_url']=$productImageUrl;
             $productDetail['url_path']="http://www.homeconnectonline.com/index.php/" . $productPageUrl;
             $productDetail['category']=$displayCatName;
-            $masterProductList[$arraycount]=$productDetail;
+            $masterProductLists[$arraycount]=$productDetail;
             $arraycount++;
         }
   
         /*echo "<pre>";
         print_r($masterProductList);
         echo "</pre>";*/
-        $masterProductList[0]=array('result'=>'success');
-         /*echo "------------------<br/><br/><br/>";*/
+        $masterProductLists[0]=array('result'=>'success');
+         //echo "------------------<br/><br/><br/>";
         //$decodedValue=json_decode($encodedValue);
         //echo "<pre>";
         //print_r($decodedValue);
@@ -80,11 +93,11 @@
     }
     catch (SoapFault $e)
     {
-        $masterProductList[0]=array('result'=>'failure');
+        $masterProductLists[0]=array('result'=>'failure');
     }
-    //echo "<pre>";
-    //print_r($masterProductList);
-    //echo "</pre>";
-    $encodedValue=json_encode($masterProductList);
+    /*echo "<pre>";
+    print_r($masterProductLists);
+    echo "</pre>";*/
+    $encodedValue=json_encode($masterProductLists);
     echo $encodedValue;
 ?>
